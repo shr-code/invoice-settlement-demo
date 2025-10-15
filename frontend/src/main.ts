b14349db9e1c77bc83922f52e1b279609ee4d6cf
Shraddha <choudharyshraddha12@gmail.com>
2025-10-15 13:25:13 +0530

    Added ag grid community version to the project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,8 @@
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+
+//  Register all community features globally
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
